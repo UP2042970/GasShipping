9c74b6bcefc89434499a2ef9aa62ffd1fe26280f
Jasser <up2042970@port.ac.uk>
2022-03-18 17:44:57 +0000

    adding new branch

---
+++ DataSetFileConvertToJASON/Helper.cs
@@ -0,0 +1,17 @@
+﻿using GasShipping.Model;
+using GasShipping.DataAgent;
+
+
+internal class Helper
+{
+    private static List<Customers> _customers;
+    private static List<Ship> _ships;
+    private static CustomerFactory _customerFactory;
+    private static ShipsFactory _shipsFactory;
+
+    public static void print(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
