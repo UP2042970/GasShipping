4a8f2f45db451be2c8341f5ebde07960a322e6e5
Jasser <up2042970@port.ac.uk>
2022-04-16 05:45:21 +0300

    Helper Class was created.... some comments were added

---
+++ GasShipping.Console/Helper.cs
@@ -0,0 +1,45 @@
+﻿using GasShipping.DataAgent;
+using GasShipping.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This willl be the main class that will be runnig
+/// the Demo
+/// </summary>
+    public static class Helper
+    {
+   
+    public static List<Ship>? Ships { get; set; }
+    /// <summary>
+    /// list of Customers objects
+    /// </summary>
+    public static List <Customers>? Ports { get; set; }
+    /// <summary>
+    /// FileAgent object
+    /// </summary>
+    public static FileAgent? FileAgent { get; set; }
+
+
+
+    /// <summary>
+    /// This is the main method that will run the whole program
+    /// </summary>
+    public static void Run()
+    {
+        //TODO: call (METHOD)  ask user for file options then populate depending on the option
+        // we will use the constant files for now
+
+        FileAgent = new (Constants.SHIPS_FILE_NAME, Constants.PATH);
+        var shipFactory=new ShipsFactory(FileAgent);
+        Ships= shipFactory.GetShipsFromFile();
+        Console.WriteLine(shipFactory.SetShipsToJSONString(Ships));
+
+
+    }
+
+}
+
--- GasShipping.DataAgent/ShipsFactory.cs
+++ GasShipping.DataAgent/ShipsFactory.cs
@@ -7,30 +7,62 @@ using System.Text.Json;
 using GasShipping.Model;
 
 namespace GasShipping.DataAgent
-{
+{/// <summary>
+/// This class will be responsible for creating ships from the file agent
+/// 
+/// </summary>
     public class ShipsFactory
     {
+        /// <summary>
+        ///  list of ship objects
+        /// </summary>
         public List<Ship> Ships { get; set; }
-        private FileAgent _fileAgent; 
+        /// <summary>
+        /// FileAgent object
+        /// </summary>
+        private FileAgent _fileAgent;
+
+        /// <summary>
+        /// Cunstrctor for ShipsFactory 
+        /// </summary>
+        /// <param name="ships">List&lt;Ship&gt;</param>
         public ShipsFactory(List<Ship> ships)
         {
             Ships = ships;
             _fileAgent = new FileAgent(Constants.SHIPS_FILE_NAME, Constants.PATH);
         }
-
+        /// <summary>
+        /// Cunstrctor for ShipsFactory 
+        /// </summary>
         public ShipsFactory()
         {
             Ships = new List<Ship>();
             _fileAgent = new FileAgent(Constants.SHIPS_FILE_NAME, Constants.PATH);
         }
+        /// <summary>
+        /// Cunstrctor for ShipsFactory 
+        /// </summary>
+        /// <param name="fileAgent">FileAgent</param>
+        public ShipsFactory(FileAgent fileAgent)
+        {
+            Ships = new List<Ship>();
+            _fileAgent = fileAgent;
+        }
 
-       
-
+        /// <summary>
+        /// This method converts List of ships to JSON string
+        /// </summary>
+        /// <returns>JSON string</returns>
         public string SetShipsToJSONString()
         {
             var opt=new JsonSerializerOptions() { WriteIndented = true };
             return JsonSerializer.Serialize<List<Ship>>(Ships, opt);
         }
+        /// <summary>
+        /// This method converts List of ships to JSON string
+        /// </summary>
+        /// <param name="ships">List&lt;Ship&gt;</param>
+        /// <returns>JSON string</returns>
         public string SetShipsToJSONString(List<Ship> ships)
         {
             if (ships is null)
@@ -41,9 +73,22 @@ namespace GasShipping.DataAgent
             var opt = new JsonSerializerOptions() { WriteIndented = true };
             return JsonSerializer.Serialize<List<Ship>>(ships, opt);
         }
+        /// <summary>
+        /// This method converts JSON string to List of ships 
+        /// </summary>
+        /// <param name="JSONstring">string</param>
+        /// <returns>List&lt;Ship&gt;</returns>
         public List<Ship> GetShipsFromJSONString(string JSONstring) => JsonSerializer.Deserialize<List<Ship>>(JSONstring);
+        /// <summary>
+        /// This method will add a ship to the List&lt;Ship&gt;
+        /// </summary>
+        /// <param name="ship">Ship</param>
         public void AddShip(Ship ship ) => Ships.Add(ship);
-
+        /// <summary>
+        /// This method will remove the ship from the List&lt;Ship&gt;
+        /// </summary>
+        /// <param name="ship">Ship</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void RemoveShip(Ship ship)
         {
             if (ship is null)
@@ -53,6 +98,10 @@ namespace GasShipping.DataAgent
 
             Ships.Remove(ship);
         }
+        /// <summary>
+        /// This method will read a JSON file then will convert it to list of ships
+        /// </summary>
+        /// <returns>List&lt;Ship&gt;</returns>
         public List<Ship> GetShipsFromFile()
         {
             var jsonString=_fileAgent.ReadFile();
